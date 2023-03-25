7d132e30142f678e29e86bafa31ca283fdc1fad3
Rotaract Club Gondomar <128552075+rotaractclubgondomar@users.noreply.github.com>
2023-03-25 11:19:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ const dislike = document.querySelector('#dislike');
 
 // constants
 const urls = [
-  'images/6.jpg',
-  'images/1.png',
   'images/2.png',
   'images/3.png',
   'images/4.png',
   'images/5.png',
+  'images/6.jpg',
 ];
 
 // variables
